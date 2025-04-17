07f88bd7ded549cd4c4909f25969444a61264e8c
adithya976 <adithyar976@gmail.com>
2025-04-17 20:56:38 +0530

    Added team members

--- landing.js
+++ landing.js
@@ -2,3 +2,16 @@ function navigateTo(folderName,filename) {
     window.location.href = `${folderName}/${filename}.html`;
   }
   
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 150, // Offset for header height
+            behavior: 'smooth'
+        });
+    });
+});
